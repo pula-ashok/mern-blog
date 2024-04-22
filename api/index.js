0c83fbf36014284dfd1d0009c90ce8eba763ca23
pula-ashok <ashokpula0307@gmail.com>
2024-04-22 14:38:47 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,10 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import createDbConnection from "./db/createDbConnection.js";
 import userRouter from "../api/routes/user.routes.js";
+import authRouter from "../api/routes/auth.routes.js";
 
 const app = express();
-dotenv.config();
 
+dotenv.config();
+app.use(express.json());
+
+app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 
 app.listen(3000, () => {
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique },
-    email: { type: String, required: true, unique },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
   { timestamps: true }
