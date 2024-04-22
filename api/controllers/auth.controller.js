8ee303cda6f8bb6a408cbde3c56909914ac8efee
pula-ashok <ashokpula0307@gmail.com>
2024-04-22 15:34:48 +0530

    add a middleware and  a function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,24 +1,31 @@
-import User from "./../models/user.model.js";
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
-export const signup = async (req, res) => {
-  const { username, email, password } = req.body;
-  if (
-    !username ||
-    !email ||
-    !password ||
-    username === "" ||
-    email === "" ||
-    password === ""
-  ) {
-    return res.status(400).json({ message: "All fields are required" });
-  }
-  //   const hashPassword = await bcryptjs.hash(password, 10);
-  const hashPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, email, password: hashPassword });
+
+export const signup = async (req, res, next) => {
   try {
-    await newUser.save();
-    return res.status(201).json("signup successfull");
+    const { username, email, password } = req.body;
+    if (
+      !username ||
+      !email ||
+      !password ||
+      username === "" ||
+      email === "" ||
+      password === ""
+    ) {
+      next(errorHandler(400, "all fields are required"));
+      return;
+    }
+    // const hashPassword = await bcryptjs.hash(password, 10);
+    const hashPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashPassword });
+    try {
+      await newUser.save();
+      res.status(201).json("signup successful");
+    } catch (error) {
+      next(error);
+    }
   } catch (error) {
-    return res.status(500).json({ message: error.message });
+    next(error);
   }
 };
--- api/index.js
+++ api/index.js
@@ -12,6 +12,16 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 app.listen(3000, () => {
   console.log("server is running at port 3000!!");
   createDbConnection();
