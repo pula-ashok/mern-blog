21227cad1b20e6d63785ef111a3d864602cd5119
pula-ashok <ashokpula0307@gmail.com>
2024-04-23 11:15:48 +0530

    complete dark mode functionality

---
+++ client/src/redux/theme/themeSlice.js
@@ -0,0 +1,15 @@
+const initialState = { theme: "light" };
+import { createSlice } from "@reduxjs/toolkit";
+
+const themeSlice = createSlice({
+  name: "theme",
+  initialState,
+  reducers: {
+    toggleTheme: (state) => {
+      state.theme = state.theme === "light" ? "dark" : "light";
+    },
+  },
+});
+
+export const { toggleTheme } = themeSlice.actions;
+export default themeSlice.reducer;
